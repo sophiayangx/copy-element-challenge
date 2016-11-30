4ea30133c75d2ffad1fa71c5eeab28a0074d3f26
sophiayangx <sopyang@nuevaschool.org>
2016-11-30 09:00:53 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,9 +5,12 @@
 // 4. copy the element and add the new element to the element list (google 'jquery add element to div')
 // 5. how do you repeat this multiple times, for multiple colors??? There is a nice data structure for this kind of thing.
 
-var elementsList = $("#elements-list")
+var elementsList = $("#elements-list");
+var elements = $(".elements");
 
- $(".element").clone().appendTo(elementsList);
+$(elements).clone().appendTo(elementsList);
+$(elements).clone().appendTo(elementsList);
+$(elements).clone().appendTo(elementsList);
 
 var color = ["red", "green", "yellow", "cornsilk", "Pink", "white", "lightpink", "pink", "coral"];
 
