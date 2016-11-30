b9472654219bffa0c8c0dedefe1330f01c854bfc
sophiayangx <sopyang@nuevaschool.org>
2016-11-30 09:28:32 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,15 @@
+// hints!
+// 1. find the element in index.html that you want to copy
+// 2. select it somehow (by class . or id #)
+// 3. google 'jquery copy element'
+// 4. copy the element and add the new element to the element list (google 'jquery add element to div')
+// 5. how do you repeat this multiple times, for multiple colors??? There is a nice data structure for this kind of thing.
+
+var elementsList = $("#elements-list");
+var element = $(".element");
+
+var colors = ["red", "green", "yellow", "cornsilk", "pink", "white", "lightpink", "pink", "coral"];
+
+colors.forEach(function (color) {
+  $(elementsList).append(element.clone().css("background-color", color))
+});
